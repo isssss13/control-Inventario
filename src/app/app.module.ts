709b00692fdc78f40777862172a03e48b5b06b57
Ivan <ivan.mcr.swami@gmail.com>
2021-02-17 23:15:51 -0600

    Se separan componentes y se agrega inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
     HeaderComponent,
     MenuComponent,
     FooterComponent,
-    SidebarComponent
+    SidebarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
