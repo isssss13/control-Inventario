a3ad11b54d2a8357b7ab9c8f8e5181dbfec55e92
Ivan <ivan.mcr.swami@gmail.com>
2021-02-18 22:39:26 -0600

    Se agrega nueva plantilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+declare var jQuery:any;
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'control de Inventario';
+
+  public mostarOcultar(){
+      $("#wrapper").toggleClass("toggled");
+  }
 }
